2d0cad5bdeb6f42ebea12413401426e078c7838d
Ajith <jose0029@e.ntu.edu.sg>
2016-01-27 18:00:35 +0000

    Adding map variables

--- mm.h
+++ mm.h
@@ -16,6 +16,7 @@ class mm
 		{'R',1}, {'G', 2},{'B',3}, {'Y',4}, {'V',5}, {'P',6},
 		{'r',7}, {'g', 8},{'b',9}, {'y',10}, {'v',11}, {'p',12}};
 	char vColors[6] = {'R','G','B','Y','V','P'};
+	int hitmap[4], map[4];
     public:
 	int play();
 	int init();
